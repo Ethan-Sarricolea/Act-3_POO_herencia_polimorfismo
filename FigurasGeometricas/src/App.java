391e427fd16fa6575864d7242913226478d8ebef
Ethan <esarricolea@gmail.com>
2024-10-23 16:24:37 -0600

    Actualizacion de notas
    Agregue notas de lo que creo que seria bueno hacer como interfaz de uso

--- FigurasGeometricas/src/App.java
+++ FigurasGeometricas/src/App.java
@@ -5,6 +5,18 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Selecciona la figura por crear \n1)Circulo 2)Rectangulo 3)Triangulo");
+        // ciclo do while
+            //println
+            // creacion de objeto o salir
+            // switch para seleccion de opcion, (area, perimetro o datos generales)
+            // arreglo o lista de figuras
     }
 }
+
+/*
+ * Aqui la idea es que se genere un do while donde se puedan crear o seleccionar figuras
+ * Aqui se genera otro do while para las eleccion de las opciones
+ * una vez creada o seleccionada una nueva figura se seleccionan las opciones de la figura (area, perimetro, geter, seter, toString)
+ * tambien dara la opcion de salir al menu principal, donde se podra crear otra figura o ver las que ya hay.
+ */
