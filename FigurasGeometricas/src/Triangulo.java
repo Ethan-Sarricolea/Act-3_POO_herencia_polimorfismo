523cdcade1390a8bb196b05cdd1fc54e036cc7c0
Ethan <esarricolea@gmail.com>
2024-10-23 17:30:02 -0600

    add triangulo

---
+++ FigurasGeometricas/src/Triangulo.java
@@ -0,0 +1,18 @@
+/*
+ * Author:
+ * Description:
+ */
+
+public class Triangulo{
+    double lado1 1.0;
+    double lado2 1.0;
+    double lado3 = 1.0;
+
+    public Triangulo(){
+        super();
+    }
+
+    public Triangulo(){
+        
+    }
+}
